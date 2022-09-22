a3e621c2c33734ad489519dce348a7a4653c629b
Indigo <indigo.crandell@gmail.com>
2022-09-22 15:37:05 -0400

    Created a new database, three tables, and seed data to fill the tables. Working well thus far.

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO department (name)
+VALUES  ('Production'),
+        ('Marketing'),
+        ('Design'),
+        ('Performace'),
+        ('Sales');
+
+INSERT INTO role (title, salary, department_id)
+VALUES  ('Director', 60000, 1),
+        ('Music Director', 60000, 1),
+        ('Costume Designer', 55000, 3),
+        ('Stitcher', 45000, 3),
+        ('Actor', 45000, 4),
+        ('Dance Captain', 48000, 4),
+        ('Social Media Manager', 45000, 2),
+        ('Box Office Manager', 48000, 5);
+
+
+INSERT INTO employee (first_name, last_name, role, manager_id)
+VALUES  ('Ginna', 'Richards', 6, null),
+        ('George', 'Hersh', 2, 1),
+        ('Marcus', 'Antonio', 5, null),
+        ('Lorraine', 'Flem', 3, null),
+        ('Abi', 'Hannigan', 4, 4);
