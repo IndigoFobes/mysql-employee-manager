9236fb8c3f8a58bf74e98477435d685403786df0
Indigo <indigo.crandell@gmail.com>
2022-09-28 14:46:12 -0400

    Finished the functions for all view options.. Now onto allowing user to insert new things...

--- index.js
+++ index.js
@@ -16,43 +16,49 @@ console.log(`Connected to database!`)
 
 // query to view all departments
 const viewDepartments = () => {
+    connection.query(
+        'SELECT id AS ID, name AS \'Department Name\' FROM department',
+        function(err, results, fields) {
+            console.table(results);
+            askQuestion();
+            //console.log(fields);
+        }
+    );
+}
 
-connection.query(
-    'SELECT id AS ID, name AS \'Department Name\' FROM department',
-    function(err, results, fields) {
-        console.table(results);
-        askQuestion();
-        //console.log(fields);
-    }
-);
+//query to view all roles
+const viewRoles = () => {
+    connection.query(
+        'SELECT role.id AS ID, title as \'Job Title\', salary as Salary, department.name AS Department FROM role JOIN department ON role.department_id=department.id;',
+        function(err, results, fields) {
+            console.table(results);
+            askQuestion();
+            //console.log(fields);
+        }
+    );
 }
 
 
-// query to view all roles
-// connection.query(
-//     'SELECT role.id AS ID, title as \'Job Title\', salary as Salary, department.name AS Department FROM role JOIN department ON role.department_id=department.id;',
-//     function(err, results, fields) {
-//         console.table(results);
-//         //console.log(fields);
-//     }
-// );
 
 // query to view all employees
+const viewEmployees = () => {
+    connection.query(
+        'select a.id as ID, CONCAT(a.first_name, \' \', a.last_name) AS Employee, role.title AS \'Job Title\', salary AS Salary, department.name AS Department, CONCAT(b.first_name, \' \', b.last_name) AS Manager from employee a JOIN role ON a.role=role.id JOIN department ON role.department_id=department.id LEFT JOIN employee b ON a.manager_id=b.id;',
+        function(err, results, fields) {
+            console.table(results);
+            askQuestion();
+            //console.log(fields);
+        }
+    );
+}
 
-// connection.query(
-//     'select a.id as ID, CONCAT(a.first_name, \' \', a.last_name) AS Employee, role.title AS \'Job Title\', salary AS Salary, department.name AS Department, CONCAT(b.first_name, \' \', b.last_name) AS Manager from employee a JOIN role ON a.role=role.id JOIN department ON role.department_id=department.id LEFT JOIN employee b ON a.manager_id=b.id;',
-//     function(err, results, fields) {
-//         console.table(results);
-//         //console.log(fields);
-//     }
-// );
 
 const question = [
     {
         name: 'home',
         type: 'list',
         message: 'What would you like to do?',
-        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee\'s role']
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee\'s role', 'Quit']
     }
 ]
 // Inquirer
@@ -67,8 +73,19 @@ inquirer
     if (response.home === 'View all departments') {
         viewDepartments();
     }
+    else if (response.home === 'View all roles') {
+        viewRoles();
+    }
+    else if (response.home === 'View all employees') {
+        viewEmployees();
+    }
+    else if (response.home === 'Quit') {
+        // Can I mak it exit the application, as in '^C'?
+        return;
+    }
 })
 .catch((error) => {
+    console.log('Something happened. OOPS.')
     // Something happened. OOPS.
     
 });
