4738354c6c54594dc9b48ea73b7d699a464649e8
Indigo <indigo.crandell@gmail.com>
2022-09-27 20:33:10 -0400

    Working example with viewDepartments. I commented the other queries out to just work on inquirer and departments.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql2');
 const cTable = require('console.table');
+const inquirer = require('inquirer');
+const { default: Choices } = require('inquirer/lib/objects/choices');
 
 // connect mysql to database
 const connection = mysql.createConnection(
@@ -13,28 +15,63 @@ console.log(`Connected to database!`)
 );
 
 // query to view all departments
+const viewDepartments = () => {
+
 connection.query(
     'SELECT id AS ID, name AS \'Department Name\' FROM department',
     function(err, results, fields) {
         console.table(results);
+        askQuestion();
         //console.log(fields);
     }
 );
+}
+
 
 // query to view all roles
-connection.query(
-    'SELECT role.id AS ID, title as \'Job Title\', salary as Salary, department.name AS Department FROM role JOIN department ON role.department_id=department.id;',
-    function(err, results, fields) {
-        console.table(results);
-        //console.log(fields);
-    }
-);
+// connection.query(
+//     'SELECT role.id AS ID, title as \'Job Title\', salary as Salary, department.name AS Department FROM role JOIN department ON role.department_id=department.id;',
+//     function(err, results, fields) {
+//         console.table(results);
+//         //console.log(fields);
+//     }
+// );
 
 // query to view all employees
-connection.query(
-    'select a.id as ID, CONCAT(a.first_name, \' \', a.last_name) AS Employee, role.title AS \'Job Title\', salary AS Salary, department.name AS Department, CONCAT(b.first_name, \' \', b.last_name) AS Manager from employee a JOIN role ON a.role=role.id JOIN department ON role.department_id=department.id LEFT JOIN employee b ON a.manager_id=b.id;',
-    function(err, results, fields) {
-        console.table(results);
-        //console.log(fields);
+
+// connection.query(
+//     'select a.id as ID, CONCAT(a.first_name, \' \', a.last_name) AS Employee, role.title AS \'Job Title\', salary AS Salary, department.name AS Department, CONCAT(b.first_name, \' \', b.last_name) AS Manager from employee a JOIN role ON a.role=role.id JOIN department ON role.department_id=department.id LEFT JOIN employee b ON a.manager_id=b.id;',
+//     function(err, results, fields) {
+//         console.table(results);
+//         //console.log(fields);
+//     }
+// );
+
+const question = [
+    {
+        name: 'home',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee\'s role']
     }
-);
+]
+// Inquirer
+const askQuestion = () => {
+inquirer
+.prompt(question)
+.then((response) => {
+
+    console.log(response.home);
+
+    // If statements to determine next function...
+    if (response.home === 'View all departments') {
+        viewDepartments();
+    }
+})
+.catch((error) => {
+    // Something happened. OOPS.
+    
+});
+};
+
+askQuestion();
